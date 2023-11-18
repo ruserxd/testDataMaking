f4c8e3516adc9c2deb3f354f740676df85b6651a
ruserxd <zxcmomo0219@gmail.com>
2023-11-18 14:01:18 +0800

    Graphy_making

--- testDataGenearte.c
+++ testDataGenearte.c
@@ -3,8 +3,32 @@
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
+
+void generateString();
+void generateINT(int);
+void generateGraphy(int, int);
+
+FILE * in;
+
+int main()
+{
+    srand(time(NULL)); // define a seed for the random number generator
+    char fileName[300];
+    printf("Please tell me the filename you want to input :");
+    scanf("%s",fileName);
+    in = fopen(fileName,"w+");
+    
+    int time = 10;
+    while (time--)
+    {
+        generateString();
+        generateINT(100);
+        fprintf(in,"\n");
+    }
+    fclose(in);
+}
 void generateString()
-{      
+{
     char random[10 + 1];
     const char ALLOWED[] = "abcdefghijklmnopqrstuvwxyz";
     int i = 0;
@@ -18,22 +42,25 @@ void generateString()
     random[10] = '\0';
     for (int i = 0; i < 3; i++)
     {
-        if (i == 0)printf("%c", toupper(random[i]));
-        else printf("%c", random[i]);
+        if (i == 0)
+            fprintf(in,"%c", toupper(random[i]));
+        else
+            fprintf(in,"%c", random[i]);
     }
 }
 void generateINT(int limit_num)
 {
     int randomNum = rand() % limit_num;
-    printf(" %d",randomNum);
+    fprintf(in," %d", randomNum);
 }
-int main()
+void generateGraphy(int column, int row)
 {
-    srand(time(NULL)); // define a seed for the random number generator
-    int time = 10;
-    while(time --) {
-        generateString();
-        generateINT(100);
-        printf("\n");
-    }
+    for (int i = 0; i < column; i++)
+    {
+        for (int k = 0; k < row; k++)
+        {
+            fprintf(in,"%c", '1');
+        }
+        fprintf(in,"\n");
+    }       
 }
